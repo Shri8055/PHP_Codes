fddfe5bfaa647c33282b3c18451d4edd09a2a0cc
Shrinivas Mahendra Kangralkar <88592860+Shri8055@users.noreply.github.com>
2024-12-18 12:16:25 +0530

    Add files via upload

---
+++ PHP_Codes/First/function.php
@@ -0,0 +1,9 @@
+<?php
+    function Happy_birthday($name,$age){
+        echo "Happy Birthday Dear {$name}<br>";
+        echo "Happy Birthday to you<br>";
+        echo "Happy Birthday Dear {$name}<br>";
+        echo "You are {$age}'s old!<br>";
+    }
+    Happy_birthday("ABC",22);
+?>
